e917d8fdd2bbb8bf51af6850b9a944a2855f6f09
dersar00 <dersar00@gmail.com>
2018-06-11 21:19:07 +0300

    added axios

--- src/App.js
+++ src/App.js
@@ -2,20 +2,79 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {SignInForm, SignUpForm, TransferForm} from './components';
+import axios from 'axios';
 const CryptoJS = require("crypto-js");
 const uuidv4 = require('uuid/v4');
 
 class App extends Component {
   state = {
-    signedUp: true
+    signedUp: true,
+    loggedIn: this.authTokenExists()
   }
 
-  signIn = () => {
-    alert("SignedIn")
+  signIn = (e) => {
+    e.preventDefault()
+
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    const request = {"user": {"email": email, "password": password}}
+    console.log(request)
+    axios.post("http://localhost:3001/api/sign_in", request)
+      .then((response) => {
+        console.log(response);
+        localStorage.setItem('authentication_token', response.data.authentication_token);
+        localStorage.setItem('email', response.data.email);
+        this.setState({loggedIn: !this.loggedIn})
+        //this.showMessage(response.data.msg)
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log(error.response);
+        //this.showError(error.response.data.error)
+      })
   }
 
-  signUp = () => {
-    alert("SignedUP")
+  signUp = (e) => {
+    e.preventDefault()
+
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    const password_confirmation = document.getElementById('confirm').value
+    const request = {"user": {"email": email, "password": password, "password_confirmation": password_confirmation}}
+    console.log(request)
+    axios.post("http://localhost:3001/api/sign_up", request)
+      .then((response) => {
+        console.log(response);
+        localStorage.setItem('authentication_token', response.data.authentication_token);
+        localStorage.setItem('email', response.data.email);
+
+        this.setState({loggedIn: !this.loggedIn})
+        //this.showMessage(response.data.msg)
+      })
+      .catch((error) => {
+        console.log(error.response);
+        //this.showError(error.response.data.error)
+      })
+  }
+
+  logout = () => {
+    const headers = {'X-USER-TOKEN': localStorage.getItem('authentication_token'), 'X-USER-EMAIL': localStorage.getItem('email')}
+    axios.delete("http://localhost:3001/api/sign_out", {headers: headers})
+     .then((response) => {
+       console.log(response)
+       localStorage.removeItem('authentication_token')
+       localStorage.removeItem('email')
+       this.setState({loggedIn: false})
+       this.setState({tokens: []})
+     })
+  }
+
+  authTokenExists () {
+    if("authentication_token" in localStorage) {
+      return true
+    } else {
+      return false
+    }
   }
 
   encrypt = () => {
